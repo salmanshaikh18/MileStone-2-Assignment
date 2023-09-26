0ea746b90f9bd00112b3876ebb8181d08abd9bca
Salman <salmanshaikh18786@gmail.com>
2023-09-26 21:49:50 +0530

    add icons and basic structure

---
+++ Project_1_PwSkillBlog/script.js
@@ -0,0 +1,6 @@
+const addIcon = document.getElementById('addIcon')
+const addBlog = document.getElementById('addBlog')
+
+addIcon.addEventListener('click', () => {
+    addBlog.style.display = 'block'
+})
