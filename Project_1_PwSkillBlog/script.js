05249f586fe99875804786bdfec112a0d3cec5de
Salman <salmanshaikh18786@gmail.com>
2023-10-02 00:04:38 +0530

    Add blog

--- Project_1_PwSkillBlog/script.js
+++ Project_1_PwSkillBlog/script.js
@@ -1,6 +1,12 @@
 const addIcon = document.getElementById('addIcon')
 const addBlog = document.getElementById('addBlog')
+const closeBlog = document.getElementById('closeBlog')
 
 addIcon.addEventListener('click', () => {
     addBlog.style.display = 'block'
+    addBlog.style.display = 'flex'
+})
+
+closeBlog.addEventListener('click', () => {
+    addBlog.style.display = 'none'
 })
