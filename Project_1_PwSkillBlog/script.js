2fc1fa2b3f5c98a764f408d8e77000c7b5bbd120
Salman <salmanshaikh18786@gmail.com>
2023-10-02 13:02:15 +0530

    update

--- Project_1_PwSkillBlog/script.js
+++ Project_1_PwSkillBlog/script.js
@@ -1,25 +1,30 @@
-const addIcon = document.getElementById('addIcon')
-const addBlog = document.getElementById('addBlog')
-const closeBlog = document.getElementById('closeBlog')
+document.addEventListener("DOMContentLoaded", function () {
+  const addIcon = document.getElementById("addIcon");
+  const addBlog = document.getElementById("addBlog");
+  const closeBlog = document.getElementById("closeBlog");
 
-addIcon.addEventListener('click', () => {
-    addBlog.style.display = 'block'
-    addBlog.style.display = 'flex'
-})
+  addIcon.addEventListener("click", () => {
+    addBlog.style.display = "block";
+    addBlog.style.display = "flex";
+  });
 
-closeBlog.addEventListener('click', () => {
-    addBlog.style.display = 'none'
-})
+  closeBlog.addEventListener("click", () => {
+    addBlog.style.display = "none";
+  });
 
-// read blog
+  // read blog
 
-const readBlogBtn = document.getElementById('readBlogBtn')
-const imgSrc = document.getElementById('blogImg')
-const blogTitle = document.getElementById('blogTitle')
-const blogDescription = document.getElementById('blogDescription')
+  const readBlogBtn = document.getElementById("readBlogBtn");
+  const imgSrc = document.getElementById("blogImg");
+  const blogTitle = document.getElementById("blogTitle");
+  const blogDescription = document.getElementById("blogDescription");
+  const title = document.getElementById("title");
+  console.log(title)
 
-readBlogBtn.addEventListener('click', () => {
-    const title = document.getElementById('title')
-    title.innerHTML = 'salman'
-    document.body.style.background = 'red'
-})
+  readBlogBtn.addEventListener("click", () => {
+    
+    // title.innerHTML = "salman";
+    console.log(title);
+    console.log("salman");
+  });
+});
