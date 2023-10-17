be2ac9c06ca1139b779484aaf9d451cccc9c03e6
Salman <salmanshaikh18786@gmail.com>
2023-10-17 21:39:51 +0530

    Optimized code

--- Project_1_PwSkillBlog/script.js
+++ Project_1_PwSkillBlog/script.js
@@ -1,3 +1,30 @@
+const readBlogBtn = document.getElementById("readBlogBtn");
+const blogPage = document.getElementById("blog-page");
+const homePage = document.getElementById("homePage");
+const backArrow = document.getElementById("backArrow");
+const addIcon = document.getElementById("addIcon");
+const addBlog = document.getElementById("addBlog");
+const closeBlog = document.getElementById("closeBlog");
+const addBlogBtn = document.getElementById("addBlogBtn");
+
+readBlogBtn.addEventListener("click", () => {
+  homePage.style.display = "none";
+  blogPage.style.display = "block";
+});
+
+backArrow.addEventListener("click", () => {
+  blogPage.style.display = "none";
+  homePage.style.display = "block";
+});
+
+addIcon.addEventListener("click", () => {
+  addBlog.style.display = "flex";
+});
+
+closeBlog.addEventListener("click", () => {
+  addBlog.style.display = "none";
+});
+
 // Load the saved blog data when the page laods
 window.addEventListener("load", () => {
   // Retrieve the saved blod from Local Storage
@@ -9,20 +36,10 @@ window.addEventListener("load", () => {
   }
 });
 
-const imgSrc = document.getElementById("imgSrc");
-console.log(imgSrc)
-const blogTitle = document.getElementById("blogTitle");
-const blogDescription = document.getElementById("blogDescription");
-const title = document.getElementById("title");
-const addBlogBtn = document.getElementById("addBlogBtn");
-const blogs = document.getElementById("blogs");
 const userUrl = document.getElementById("userUrl");
 const userTitle = document.getElementById("userTitle");
 const userDescription = document.getElementById("userDescription");
 const userTextarea = document.getElementById("userTextarea");
-const description = document.getElementById('description')
-const textarea = document.getElementById('textarea')
-const readBlogBtn = document.getElementById('readBlogBtn')
 
 addBlogBtn.addEventListener("click", () => {
   // create a blog object to store the data
@@ -35,7 +52,7 @@ addBlogBtn.addEventListener("click", () => {
 
   // Retrieve the saved blogs from Local Storage
   const savedBlogs = JSON.parse(localStorage.getItem("blogs")) || [];
-  console.log(savedBlogs)
+  console.log(savedBlogs);
 
   // Add the new blog to the array of saved blogs
   savedBlogs.push(blogData);
@@ -50,9 +67,7 @@ addBlogBtn.addEventListener("click", () => {
   userUrl.value = "";
   userTitle.value = "";
   userDescription.value = "";
-  description.value = "",
-
-  addBlog.style.display = "none";
+  (userTextarea.value = ""), (addBlog.style.display = "none");
 });
 
 const createBlogElement = (blogData) => {
@@ -85,11 +100,9 @@ const createBlogElement = (blogData) => {
   blog.appendChild(button);
 
   const deleteBlog = document.createElement("button");
-  deleteBlog.setAttribute("id", "deleteBlog");
+  deleteBlog.setAttribute("id", "deleteBlogBtn");
   deleteBlog.innerHTML = "Delete Blog";
   blog.appendChild(deleteBlog);
-  deleteBlog.style.background = "rgb(233, 85, 85)";
-  deleteBlog.style.color = "#fff";
 
   // Add event listener to the delete button
   deleteBlog.addEventListener("click", () => {
@@ -107,46 +120,17 @@ const createBlogElement = (blogData) => {
     blogPage.style.display = "block";
     title.innerText = blogData.title; // Set the title to the specific blog's title
     description.innerText = blogData.description; // Set the blog description to the specific blog's description
-    imgSrc.src = blogData.url // Update the image src
-    textarea.innerHTML = blogData.textarea
+    imgSrc.src = blogData.url; // Update the image src
+    textarea.innerHTML = blogData.textarea;
   });
-
-  
 };
 
-const blogImg = document.getElementById('blogImg')
-// const blogDescription = document.getElementById('blogDescription')
-// const blogTitle = document.getElementById('blogTitle')
+const blogImg = document.getElementById("blogImg");
 
-readBlogBtn.addEventListener('click', () => {
+readBlogBtn.addEventListener("click", () => {
   homePage.style.display = "none";
-    blogPage.style.display = "block";
-    title.innerText = blogTitle.innerText // Set the title to the specific blog's title
-    description.innerText = blogDescription.innerText // Set the blog description to the specific blog's description
-    imgSrc.src = blogImg.src // Update the image src
-    // textarea.innerHTML = 
-})
-
-
-
-const addIcon = document.getElementById("addIcon");
-const addBlog = document.getElementById("addBlog");
-const closeBlog = document.getElementById("closeBlog");
-
-const homePage = document.getElementById("homePage");
-const blogPage = document.getElementById("blog-page");
-
-addIcon.addEventListener("click", () => {
-  addBlog.style.display = "block";
-  addBlog.style.display = "flex";
-});
-
-closeBlog.addEventListener("click", () => {
-  addBlog.style.display = "none";
-});
-
-const backArrow = document.getElementById("backArrow");
-backArrow.addEventListener("click", () => {
-  homePage.style.display = "block";
-  blogPage.style.display = "none";
+  blogPage.style.display = "block";
+  title.innerText = blogTitle.innerText; // Set the title to the specific blog's title
+  description.innerText = blogDescription.innerText; // Set the blog description to the specific blog's description
+  imgSrc.src = blogImg.src; // Update the image src
 });
