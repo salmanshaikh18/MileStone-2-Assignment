d97d8adfd9369cf0d6900c288aceda96b7603614
Salman <salmanshaikh18786@gmail.com>
2023-10-22 15:19:00 +0530

    project 2

--- Project_2_MovieSearchApp/script.js
+++ Project_2_MovieSearchApp/script.js
@@ -1,45 +1,43 @@
-const searchInput = document.querySelector('input')
-const apiKey = 'd0f37330'; // Replace with your actual API key
-// const movieDetails = document.getElementById('movieDetails');
-const movieDetails = document.querySelector('.box');
-const movieName = document.getElementById('movieName')
-const movieImg = document.getElementById('movieImg')
+const searchInput = document.querySelector("input");
+const main = document.querySelector("main");
 
-searchInput.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        const searchTerm = searchInput.value;
-    if (searchTerm) {
-        fetch(`https://www.omdbapi.com/?t=${searchTerm}&apikey=${apiKey}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.Response === 'True') {
-                    console.log(data)
-                    // movieDetails.innerHTML = `
-                    //     <h2>${data.Title}</h2>
-                    //     <img src="${data.Poster}" alt="${data.Title} Poster">
-                    //     <p>Year: ${data.Year}</p>
-                    //     <p>Director: ${data.Director}</p>
-                    //     <p>Plot: ${data.Plot}</p>
-                    // `;
-                    movieName.innerHTML = data.Title
-                    movieImg.src = data.Poster
-                } else {
-                    movieDetails.innerHTML = 'Movie not found.';
-                    setTimeout(() => {
-                        window.location.reload()
-                    }, 3000)
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                movieDetails.innerHTML = 'An error occurred while fetching data.';
-                setTimeout(() => {
-                    window.location.reload()
-                }, 3000)
-            });
-    }
-    }
-})
+const searchMovies = () => {
+  main.innerHTML = ""; // Clear the previous search results;
+  const inputMovie = document.getElementById("inputMovie").value;
+  const movieBox = document.querySelector(".box");
+  const apiKey = "d0f37330";
+  if (inputMovie) {
+    fetch(`http://www.omdbapi.com/?s=${inputMovie}&apikey=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.Search);
+        if (data.Search) {
+          data.Search.forEach((movie) => {
+            const movieBox = document.createElement("div");
+            movieBox.setAttribute("class", "box");
 
+            movieBox.innerHTML = `
+                <div class="imgBox">
+                <img id="movieImg" src="${movie.Poster}" alt="movieImg">
+                </div>
+                <h2 id="movieName">"${movie.Title}"</h2>
+                <button id="watchNowBtn">"Watch Now"</button>
+            `;
 
+            document.querySelector("main").appendChild(movieBox);
+          });
+        } else {
+          movieBox.innerHTML = "Movie Not Found!";
+        }
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  }
+};
 
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    searchMovies();
+  }
+});
